b880c3b048120acc454de39f1635a0a85a6d44fd
Gabriela <gabysimon97@gmail.com>
2019-05-18 11:03:51 +0200

    Creado el proyecto base de Django

---
+++ django.py
@@ -0,0 +1,12 @@
+#akjds
+if 3 < 2:
+   print("Hello Django")
+def hi(name):
+    print("Hi " + name)
+    print("How are you?")
+hi("Gabriela")
+#bucle
+
+girls = ["Rachel", "Rosa", "Sofia"]
+for name in girls:
+    hi(name)
